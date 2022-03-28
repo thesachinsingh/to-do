427509a645a3cb937cef95aaaf5baf222343a1e9
Sachin Singh <49466686+thesachinsingh@users.noreply.github.com>
2022-03-28 23:38:15 +0530

    added few more routes

--- application/controllers.py
+++ application/controllers.py
@@ -1,11 +1,13 @@
 # from flask import current_app as app
 # from flask import redirect, render_template, session
 # from application.models import db, users, notes
+from datetime import datetime
+from email import message
 from flask import current_app as app, redirect
 from flask import request, session, render_template, flash
 from application.models import db, users, notes
 from flask_bcrypt import Bcrypt
-import arrow
+
 
 bcrypt = Bcrypt(app)
 
@@ -32,11 +34,12 @@ def home():
         if current_user:
             user_todo = notes.query.filter_by(user_id = current_user.user_id)
             #if to-do exists for that user
-            return render_template('home.html', user_todo = user_todo)
+            return render_template('home.html', user_todo = user_todo, logged_in = True)
     # return render_template('home.html', message = message)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    message = ""
     if request.method == 'POST':
         username = request.form['username']
         user = users.query.filter_by(username = username).first()
@@ -45,20 +48,35 @@ def login():
             if bcrypt.check_password_hash(pw_hash, request.form['password']):
                 session['username'] = username
                 return redirect('/')
+        else:
+            message = "Incorrect credentials"
+    elif "username" in session:
+        return redirect('/')
+    
+    return render_template('login.html', message = message)
 
-
-    return render_template('login.html')
+@app.route('/logout')
+def logout():
+    if "username" in session:
+        session.pop("username")
+        return redirect('/')
 
 @app.route('/signup', methods = ['POST', 'GET'])
 def sign_up():
     if request.method == 'POST':
         username = request.form['username']
-        pw_hash = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
-        user = users(username = username, password = pw_hash)
-        db.session.add(user)
-        db.session.commit()
-        return redirect('/login')
-
+        userhai = users.query.filter_by(username = username).first()
+        if not userhai:
+            pw_hash = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+            user = users(username = username, password = pw_hash)
+            db.session.add(user)
+            db.session.commit()
+            return redirect('/login')
+        else:
+            message = "Create a unique username, this username already exists"
+            return render_template('register.html', message = message) 
+    elif "username" in session:
+        return redirect('/')
     return render_template('register.html')
 
 @app.route('/create', methods = ['POST', 'GET'])
@@ -67,11 +85,12 @@ def create_todo():
         user_id =  users.query.filter_by(username = session["username"]).first().user_id
         heading = request.form["heading"]
         description = request.form["description"]
-        created_date = arrow.utcnow()
+        created_date = datetime.utcnow()
         completed = False
         todo = notes(user_id = user_id, heading = heading, description = description, created_on = created_date, last_updated = created_date, completed = completed)
         db.session.add(todo)
         db.session.commit()
+        return redirect('/')
 
 
 
